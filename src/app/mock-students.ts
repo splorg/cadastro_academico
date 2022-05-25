82642ba940f2dab2d93aa179e4a9f5e1210b1bee
gabriel.lima <gabriel.limabs02@gmail.com>
2022-05-25 20:00:16 -0300

    fixed typo

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -3,7 +3,7 @@ import { Student } from './student'
 export const STUDENTS: Student[] = [
     { id: 1, name: 'Zezinho', age: 19, course: 'Análise e Desenvolvimento de Sistemas', semester: 3 },
     { id: 2, name: 'Luisinho', age: 18, course: 'Gestão de Tecnologia da Informação', semester: 1 },
-    { id: 3, name: 'Miguelito', age: 19, course: 'Mecatrônic', semester: 6 },
+    { id: 3, name: 'Miguelito', age: 19, course: 'Mecatrônica', semester: 6 },
     { id: 4, name: 'Joselito', age: 19, course: 'Gestão Empresarial', semester: 2 },
     { id: 5, name: 'Pedrinho', age: 19, course: 'Análise e Desenvolvimento de Sistemas', semester: 2 },
     { id: 6, name: 'Joãosinho', age: 19, course: 'Análise e Desenvolvimento de Sistemas', semester: 3 }
